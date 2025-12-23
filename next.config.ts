8e94716c42bbf17e3dd634193fc901a4816e5205
vdkhoi20 <vdkhoi.itai@gmail.com>
2025-12-23 13:11:44 +0700

    ok

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "export",
+  basePath: "/CPAM.github.io",
+  assetPrefix: "/CPAM.github.io/",
   images: {
     unoptimized: true,
   },
